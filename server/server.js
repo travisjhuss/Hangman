9f17b8284aa1c43185a670c754827618669bea53
Travis Huss <travisjhuss@gmail.com>
2021-01-03 17:14:25 -0600

    movieTitle array created and variable randomMovie to generate title

--- server/server.js
+++ server/server.js
@@ -9,8 +9,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // serve back static files
 app.use(express.static('server/public'));
 
+let movieTitles = [
+    'The Shining',
+    'There Will Be Blood',
+    'Fargo',
+    'The Godfather',
+    'Star Wars',
+    'The Lord of the Rings',
+    'Do the Right Thing',
+    'The Dark Knight',
+    'The Avengers'
+];
 
+let randomMovie = movieTitles[Math.floor(Math.random() * movieTitles.length)];
 
+console.log(randomMovie);
 
 
 // server
